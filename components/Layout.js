6521f553155b39e87a5575d593ca820e1e0d982e
valdemar <valdemar@3x00.net>
2023-03-28 11:50:42 +0300

    Added favicon and page title

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,16 @@
 import styles from "@/styles/Layout.module.css";
+import Head from "next/head";
 
 export default function Layout(props) {
   return(
     <>
+      <Head>
+        <title>PassSafe Manager</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+      </Head>
       <main>
         {props.children}
       </main>
